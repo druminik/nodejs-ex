84d0f9ed1a340c99fa38cc2595b48f3df0de7ec7
Dominik Liebmann <dominik_l@gmx.net>
2019-09-24 13:25:48 +0200

    updated port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
+var port = 8080;
 
-app.listen(3000, () => {
- console.log("Server running on port 3000");
+app.listen(port, () => {
+ console.log("Server running on port %s",port);
 });
