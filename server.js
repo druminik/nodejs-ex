f6a5d5fe10c133cf1c8c04c6134a84a58e6a3bad
Dominik Liebmann <dominik_l@gmx.net>
2019-09-24 13:29:17 +0200

    added resource

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var express = require("express");
 var app = express();
 var port = 8080;
 
+app.get("/", (req, res, next) => {
+    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+   });
+
 app.listen(port, () => {
  console.log("Server running on port %s",port);
 });
